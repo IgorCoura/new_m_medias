24fe1757fcf42fb96ff0e908396c12dffb81e6ee
Igor de Brito Coura <igor-coura@hotmail.com>
2021-09-09 09:23:46 -0300

    cors resolved

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:new_m_medias/api/maua_api.dart';
-import 'package:new_m_medias/models/student_model.dart';
-import 'package:new_m_medias/utilities/temp_login.dart';
 
 class LoginModel {
   Future<Response> login(TextEditingController ctrlEmail,
